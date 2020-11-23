8d2343eefd3748ef0f2010e848a5c8f7e98064fe
Matt Harrison <matt@harrison.us.com>
2020-11-23 17:11:39 -0500

    implement working version of sink controller

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::process::Command;
+
+use pulsectl::controllers::AppControl;
+use pulsectl::controllers::DeviceControl;
+use pulsectl::controllers::SinkController;
+
+fn mpd_wait() {
+    Command::new("mpc")
+        .arg("idle")
+        .status()
+        .expect("failed to execute process");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut handler = SinkController::create();
+
+    loop {
+        let device_name = "alsa_output.pci-0000_00_14.2.analog-stereo";
+        let mpd_name = "ALSA plug-in [mpd]";
+
+        let device = handler.get_device_by_name(device_name).ok();
+        if device.is_none() {
+            println!("Playback Device {} not found", device_name);
+            continue;
+        }
+        let applications = handler
+            .list_applications()
+            .expect("Error getting application list");
+
+        let mpd = applications
+            .iter()
+            .find(|&app| app.proplist.get_str("application.name").unwrap() == mpd_name);
+        if mpd.is_none() {
+            continue;
+        }
+        match handler.move_app_by_index(mpd.unwrap().index, device.unwrap().index) {
+            Err(_e) => continue,
+            _ => (),
+        }
+
+        mpd_wait();
+    }
 }
